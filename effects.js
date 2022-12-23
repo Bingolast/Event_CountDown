79e8a059b5d2e6aeff66561eb671859db6ca9cb3
Pranav Prajapati <pranavprajapati586@gmail.com>
2022-12-23 23:00:19 +0530

    added css html of timer

---
+++ effects.js
@@ -0,0 +1,21 @@
+let days = document.getElementById('days');
+let hours = document.getElementById('hours');
+let minutes = document.getElementById('minutes');
+let seconds = document.getElementById('seconds');
+
+let dd = document.getElementById('dd');
+let hh = document.getElementById('hh');
+let mm = document.getElementById('mm');
+let ss = document.getElementById('ss');
+
+let day_dot = document.querySelector('.day_dot');
+let hr_dot = document.querySelector('.hr_dot');
+let min_dot = document.querySelector('.min_dot');
+let sec_dot = document.querySelector('.sec_dot');
+
+let endDate = '01/01/2023 00:00:00';
+// date format mm/dd/yy
+
+let x = setInterval(function(){
+    
+})
